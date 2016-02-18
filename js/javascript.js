9e3ebd2496f5687f57730f9bdf254e1f76c079bb
cxj10023 <cxj10023@outlook.com>
2016-02-18 10:15:09 +0800

    Add detail

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@ window.onload = function (){
 	    b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->'
 	    return b.getElementsByTagName('i').length === 1
 	}
-	if(isIE(8) || isIE(7) || isIE(6) || isIE(5) || isIE(4) || isIE(3) || isIE(2) || isIE(1)){
-	    alert('本页面仅兼容IE9+浏览器');  
+	if(isIE(7) || isIE(6) || isIE(5) || isIE(4) || isIE(3) || isIE(2) || isIE(1)){
+	    alert('本页面仅兼容IE8+浏览器');  
 	}
 }
